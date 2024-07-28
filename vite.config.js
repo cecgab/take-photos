d63505cc61685935c79796fdc09a34e1fd5a1b61
Gabriele Ceccarelli <41048900+cecgab@users.noreply.github.com>
2024-07-28 15:35:26 +0200

    set 'base' to define config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/take-photos/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
